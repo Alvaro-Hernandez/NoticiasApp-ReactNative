03d422de1f278aa4e28986a6ca87048ddfbfcb06
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-06-15 14:23:43 -0600

    Uso de Hooks y creacion de funcion asincrona v2

--- src/NewsList.js
+++ src/NewsList.js
@@ -1,3 +1,25 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
 import axios from 'axios';
+
+const NewList = ({navegation}) => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  const fetchNews = async () => {
+    try {
+      const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+        params: {
+          country: 'us',
+          apiKey: 'cb9dedf4ef9344a58373f12d94aefd92',
+        },
+      });
+      setNews(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
